edf0e7d7036b247608d837a0a5b604f13a102458
Javier-201906795 <2993047870101@ingenieria.usac.edu.gt>
2024-07-27 11:17:48 -0600

    Testing wweb js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Create a new client instance
+//const client = new Client();
+const client = new Client({
+    puppeteer:{
+        headless:false,
+    },
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+// Start your client
+client.initialize();
+
+
